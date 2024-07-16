456cb0d3ce4fcda004ffa29a69420425e76c1b1a
llupanar <71102491+llupanar@users.noreply.github.com>
2024-07-16 11:53:13 +0300

    init

---
+++ crud-app/src/test/java/org/jpract/crudapp/CrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jpract.crudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
